2a7761fc6d13618956c2eb732b67380f69aceb0e
AnetaEva <60235389+AnetaEva@users.noreply.github.com>
2020-10-28 10:44:30 -0400

    Created Reporting Class

---
+++ src/edu/psu/ist420/Reporting.java
@@ -0,0 +1,35 @@
+package edu.psu.ist420;
+
+public class Reporting {
+
+    public void daysAbsent{
+        //employeename
+        //days off
+        //days worked
+    }
+
+    public void overtimeHours{
+        //employee name
+        //daily hours worked
+        //overtime hours per month
+        //overtime hours per year
+    }
+
+    public void partTimeEmployees{
+
+    }
+
+    public void fullTimeEmployees{
+
+    }
+
+    public void maleToFemaleRatio{
+
+    }
+
+    public void lengthOfTimeToQuit{
+
+    }
+
+
+}
